d8b4b46f4e4e18b4821882fe2ac8b1ccca919d95
Raphael Araújo <raphael.araujo@nodo.cc>
2018-06-02 15:42:37 -0400

    First commit

---
+++ app501px/Helpers/Constants.swift
@@ -0,0 +1,20 @@
+//
+//  Constants.swift
+//  app501px
+//
+//  Created by Raphael Araújo on 2018-05-31.
+//  Copyright © 2018 Raphael Araújo. All rights reserved.
+//
+
+import Foundation
+
+enum API {
+    enum Credentials {
+        static let ConsumerKey = "rfLAmy3OVrS9VXQZle6fJDnHP9Wug2PBjWJtgW2H"
+        static let ConsumerSecret = "16YUQLdu1O6thmlCa6ULkNVfYUbeqcDTsLlLgz4S"
+    }
+    enum Endpoints {
+        static let APIBaseURL = "https://api.500px.com/v1"
+        static let RequestToken = "oauth/request_token"
+    }
+}
